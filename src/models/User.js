7f3928cb8cc43283cc43f99c36a6b138f0d45910
Marlxton <skendertoni2@gmail.com>
2022-07-19 16:01:22 +0200

    push

---
+++ src/models/User.js
@@ -0,0 +1,9 @@
+export class User {
+  constructor({ displayName }) {
+    this.displayName = displayName;
+    this.avatarText = this.displayName.split(' ')
+      .map((word) => word[0])
+      .join('');
+    this.avatarBackgroundColor = `hsl(${Math.round(Math.random() * 360)}, 75%, 75%)`;
+  }
+}
---
+++ src/pages/ChatPage/ChatPage.component.jsx
@@ -0,0 +1,46 @@
+import "./ChatPage.styles.scss";
+
+import { Message as MessageModel } from "../../models/Message";
+import { Message } from "../../components/Message";
+import { MessageForm } from "../../components/MessageForm";
+import { useUser } from "../../contexts/UserContext";
+import { useState } from "react";
+
+export function ChatPage() {
+  const { user } = useUser();
+  const [state, setState] = useState([]);
+
+  const sendMessage = (formState) => {
+    const message = new MessageModel({
+      messageText: formState.message,
+      user,
+    });
+
+    setState((state) => [ ...state, message ]);
+  }
+
+  const messageItems = state.map((message, index) => (
+    <div key={index} className="chat-page__message-list-item">
+      <Message
+        avatarBackgroundColor={message.user.avatarBackgroundColor}
+        avatarText={message.user.avatarText}
+        displayName={message.user.displayName}
+        time={message.displayCreatedAt()}
+      >
+        {message.messageText}
+      </Message>
+    </div>
+  ));
+
+  return (
+    <div className="chat-page">
+      <div className="chat-page__title">Chat with friends</div>
+      <div className="chat-page__message-list">
+        {messageItems}
+      </div>
+      <div className="chat-page__form">
+        <MessageForm onSend={sendMessage} />
+      </div>
+    </div>
+  );
+}
