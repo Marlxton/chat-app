bf610bc9eed26fca687cef46b5729ec6a0994de3
Marlxton <skendertoni2@gmail.com>
2022-07-19 18:53:17 +0200

    set up scaledrone

--- src/pages/ChatPage/ChatPage.component.jsx
+++ src/pages/ChatPage/ChatPage.component.jsx
@@ -5,10 +5,12 @@ import { Message } from "../../components/Message";
 import { MessageForm } from "../../components/MessageForm";
 import { useUser } from "../../contexts/UserContext";
 import { useState } from "react";
+import { useEffect } from "react";
 
 export function ChatPage() {
   const { user } = useUser();
   const [state, setState] = useState([]);
+  const [drone, setDrone] = useState(null);
 
   const sendMessage = (formState) => {
     const message = new MessageModel({
@@ -16,8 +18,40 @@ export function ChatPage() {
       user,
     });
 
-    setState((state) => [ ...state, message ]);
+    if (drone !== null) {
+      drone.publish({
+        room: 'chat',
+        message: message
+      });
+    }
   }
+
+  useEffect(() => {
+    if (drone !== null) return;
+    setDrone(new window.Scaledrone('xcvFGQQkEjF3XGHs'));
+  }, [drone, setDrone]);
+
+  useEffect(() => {
+    if (drone === null) return;
+
+    const room = drone.subscribe('chat');
+
+    room.on('open', error => {
+      if (error) {
+        return console.error(error);
+      }
+      console.log('Connected to room');
+    });
+  
+    room.on('message', message => {
+      console.log('Message received', message);
+
+      setState((state) => [
+        ...state,
+        MessageModel.fromObject(message.data)
+      ]);
+    });
+  }, [drone]);
 
   const messageItems = state.map((message, index) => (
     <div key={index} className="chat-page__message-list-item">
